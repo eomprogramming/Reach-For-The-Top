d890baa865d66314e4700c920fa431d9254d0e2f
Aly <aly.hassan9@gmail.com>
2012-01-31 00:06:58 -0500

    The frame now shows up, but it is empty.

--- src/reachForTheTop/GameFrame.java
+++ src/reachForTheTop/GameFrame.java
@@ -6,7 +6,10 @@ public class GameFrame extends JFrame{
 	
 	public GameFrame(){
 		
-		super("Reach For The Top!");
+		super("Reach For The Top!");		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300,300);
+		setVisible(true);
 	}
 
 }
--- src/reachForTheTop/Launcher.java
+++ src/reachForTheTop/Launcher.java
@@ -2,6 +2,7 @@ package reachForTheTop;
 
 public class Launcher {
 	public static void main(String args[]){
+		new GameFrame();
 		System.out.println("Launched");
 	}
 }
