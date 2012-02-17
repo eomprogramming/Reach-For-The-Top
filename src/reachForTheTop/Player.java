482b488feaff1e3b732c024d0c1116e1bd67765b
Hao Wei <hao5838@hotmail.com>
2012-02-16 23:23:21 -0500

    added player class

---
+++ src/reachForTheTop/Player.java
@@ -0,0 +1,58 @@
+package reachForTheTop;
+
+public class Player {
+	private String name;
+	private int score;
+	
+	public Player(String name)
+	{
+		setName(name);
+		resetScore();
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void resetScore()
+	{
+		score = 0;
+	}
+	
+	public void increaseScore(int a)
+	{
+		score += a;
+	}
+	
+	public int getScore()
+	{
+		return score;
+	}
+	
+	/**
+	 * if file with student name does not exist
+	 * {
+	 * 		create a new name.reach files and open it
+	 * }
+	 * else
+	 * {
+	 * 		open existing name.reach files
+	 * }
+	 * 
+	 * write out the score and update the number or rounds played and the total score for the student
+	 * include the data
+	 * 
+	 * follow the format in the example .reach files
+	 * close file
+	 */
+	public void saveScore()
+	{
+		
+	}
+}
