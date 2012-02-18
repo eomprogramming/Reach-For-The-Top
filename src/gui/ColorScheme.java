740608bd8d0e01e6828c1c2a238a7ebffc9c996c
Aly <aly.hassan9@gmail.com>
2012-02-18 14:50:00 -0500

    Added class to handle the color scheme

---
+++ src/gui/ColorScheme.java
@@ -0,0 +1,11 @@
+package gui;
+
+import java.awt.Color;
+
+public class ColorScheme {
+	
+	public final static Color DEFAULT_MAIN = new Color(0,0,0);
+	public final static Color DEFAULT_HIGHLIGHT = new Color(0,0,0);
+	public final static Color DEFAULT_SECONDARY = new Color(0,0,0);	
+	
+}
