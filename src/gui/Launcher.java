16360d8991b152feffa499f2d8e6c17ecdb47d82
Aly <aly.hassan9@gmail.com>
2012-02-29 18:02:37 -0500

    Added a comment, see you can still commit!

--- src/gui/Launcher.java
+++ src/gui/Launcher.java
@@ -1,7 +1,8 @@
 package gui;
 import reachForTheTop.PlayerIO;
-
-
+/**
+ * Launches the game.
+ */
 public class Launcher {
 	public static void main(String args[]){
 		PlayerIO.makeScoreFolder();	   
